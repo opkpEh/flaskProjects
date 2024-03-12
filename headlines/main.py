c25df98a3a5c47d5ac6f0b9bbe14ca36f58ffb09
opkpEh <harshpandeykp20@gmail.com>
2024-03-12 14:33:23 +0530

    hyperlinks

--- headlines/main.py
+++ headlines/main.py
@@ -8,18 +8,15 @@ RSS_FEED= {
         "fox": "https://moxie.foxnews.com/google-publisher/latest.xml",
         "db": "https://www.bhaskar.com/rss-v1--category-4587.xml",
 }
+
 def get_news(publication):
     feed= feedparser.parse(RSS_FEED[publication])
     first_article= feed['entries'][0]
-    return render_template("index.html",headline=publication, articles=feed['entries'])
+    return render_template("headline.html",headline=publication, articles=feed['entries'])
 
 @app.route("/")
 def index():
-    return """<pre><h1 style= "color: red" >Home page to get RRS feed from: 
-bbc 
-cnn 
-fox
-db</h1></pre>"""
+    return render_template("index.html") 
 
 @app.route("/<publication>")
 def get_news_publication(publication):
