00e32a3f6a8a2d47784b9cd057cb54ee20c530ae
opkpEh <harshpandeykp20@gmail.com>
2024-03-12 14:12:09 +0530

    larger feed

--- headlines/main.py
+++ headlines/main.py
@@ -1,26 +1,17 @@
 import feedparser 
-from flask import Flask
+from flask import Flask, render_template
 
 app= Flask(__name__)
 
 RSS_FEED= {
         "bbc": "http://feeds.bbci.co.uk/news/rss.xml",
-        "cnn": "http://rss.cnn.com/rss/edition.rss",
         "fox": "https://moxie.foxnews.com/google-publisher/latest.xml",
         "db": "https://www.bhaskar.com/rss-v1--category-4587.xml",
 }
 def get_news(publication):
     feed= feedparser.parse(RSS_FEED[publication])
     first_article= feed['entries'][0]
-    return f"""<html>
-                <body>
-                    <h1>{publication} Headlines </h1>
-                    <b> {first_article.get("title")}</b> <br/>
-                    <i> {first_article.get("published")}</i> <br/>
-                    <p> {first_article.get("summary")}</p> <br/>
-                </body>
-              </html> """
-
+    return render_template("index.html",headline=publication, articles=feed['entries'])
 
 @app.route("/")
 def index():
